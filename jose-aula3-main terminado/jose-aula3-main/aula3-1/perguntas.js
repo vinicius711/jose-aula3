9af8b28cdbc87f3690b35afcba9e71fd82cb1ed4
vinicius711 <pereira.vinicius18@escola.pr.gov.br>
2025-08-14 11:53:19 -0300

    Add files via upload

---
+++ jose-aula3-main terminado/jose-aula3-main/aula3-1/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'qual foi a primeira coisa descoberta no mundo',
+    'A primeira coisa descoberta pelo ser humano foi o fogo'
+)
+
+criaCartao(
+    'historia',
+    'qual foi a primeira pessoa a pisar na lua',
+    'A primeira pessoa a pisar na Lua foi o astronauta americano Neil Armstrong'
+)
+
+criaCartao(
+    'jogos',
+    'qual foi a primero jogo ser lançado',
+    'O primeiro jogo eletrônico criado para entretenimento foi Tennis for Two'
+)
+
+criaCartao(
+    'Lingua japonesa',
+    'como diz oi em japonês',
+    'A forma mais comum de dizer "olá" em japonês é Konnichiwa'
+)
